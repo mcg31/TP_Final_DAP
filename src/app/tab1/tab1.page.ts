1696c55c5c0349b7a7d7cd3a379b6297ba44de37
mcg31 <mcg-pna@gmail.com>
2020-10-12 02:07:18 -0300

    TercerCommit:TP_Final_pasandoA_APIrest

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,13 +8,14 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
   user: string = "";
-  private contrasenia: string;
+  /*private contrasenia: string;
   private usuarios: [{ "Nombre": "Ignacio", "contraseña": "Ignacio" }, { "Nombre": "José González", "contraseña": "JoseG" }, { "Nombre": "José Brizio", "contraseña": "JoseB" }];
-  
+  */
   constructor(private alContrl: AlertController) { }
-  
-  public login() {
-    for (var i; i < 3; i++) {
+  /*
+  public async login() {
+    let verificador: boolean = false;
+    for (var i = 0; i < 3; i++) {
       
       if (this.usuarios[i].Nombre == this.user && this.usuarios[i].contraseña == this.contrasenia) {
         const cuerpoAlerta = {
@@ -22,8 +23,20 @@ export class Tab1Page {
           message: "Ingreso exitoso como usuario: " + this.user,
           buttons: ["OK"]
         };
-        const alerta = this.alContrl.create(cuerpoAlerta);
+        verificador = true;
+        const alerta = await this.alContrl.create(cuerpoAlerta);
+        await alerta.present();
       }
     }
+    if (!verificador) {
+      const cuerpoAlertaF = {
+        header: "Ingreso",
+        message: "Verifique los datos ingresados",
+        buttons: ["OK"]
+      };
+      const alertaF = await this.alContrl.create(cuerpoAlertaF);
+      await alertaF.present();
+    }
   }
+  */
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,43 +9,23 @@ import { Platform } from '@ionic/angular';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-  selectedDateI:string = ""; 
-  selectedDateF:string = ""; 
-  selectedDate:string = "";
-  constructor(public datePicker: DatePicker, public datePipe: DatePipe, public platform: Platform)
-  { 
-    this.platform.ready().then(()=>{
-      this.selectedDateI = this.datePipe.transform(new Date(),"dd-MM-yyyy");
-      this.selectedDateF = this.datePipe.transform(new Date(),"dd-MM-yyyy");
-    })
-  }
-  
-  SelectDate()
-  {
-    var options = {
-      date:new Date(),
-      mode:'date',
-      androidTheme:this.datePicker.ANDROID_THEMES.THEME_TRADITIONAL   
-    }
-    this.datePicker.show(options).then((date) => {
-      this.selectedDate = this.datePipe.transform(date, "dd-MM-yyyy");
 
-    })
-  }
-  
-//POR ORDEÑE  
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: '1er ordeñe'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: '2do ordeñe'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: '3er ordeñe'}
+
+export class Tab3Page {
+  fechaI = new Date();
+  fechaF = new Date();
+  //POR ORDEÑE  
+  public lineChartData: Array<any> = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: '1er ordeñe' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: '2do ordeñe' },
+    { data: [18, 48, 77, 9, 100, 27, 40], label: '3er ordeñe' }
   ];
-  public lineChartLabels:Array<any> = ['día 1', 'día 2', 'día 3', 'día 4', 'día 5', 'día 6', 'día 7'];
-  public lineChartOptions:any = {
+  public lineChartLabels: Array<any> = ['día 1', 'día 2', 'día 3', 'día 4', 'día 5', 'día 6', 'día 7'];
+  public lineChartOptions: any = {
     responsive: true
   };
-  public lineChartColors:Array<any> = [
-    { 
+  public lineChartColors: Array<any> = [
+    {
       backgroundColor: 'rgba(255,255,255,1)',
       borderColor: 'rgba(255,0,0,1)',
       pointBackgroundColor: 'rgba(255,0,0,1)',
@@ -53,7 +33,7 @@ export class Tab3Page {
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
-    { 
+    {
       backgroundColor: 'rgba(255,255,255,1)',
       borderColor: 'rgba(0,255,0,1)',
       pointBackgroundColor: 'rgba(0, 255,0,1)',
@@ -61,7 +41,7 @@ export class Tab3Page {
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(77,83,96,1)'
     },
-    { 
+    {
       backgroundColor: 'rgba(255,255,255,1)',
       borderColor: 'rgba(0,0,255, 1)',
       pointBackgroundColor: 'rgba(0,0,255, 1)',
@@ -70,28 +50,37 @@ export class Tab3Page {
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
   ];
-  public lineChartLegend:boolean = true;
+  public lineChartLegend: boolean = true;
   public lineChartType: string = 'line';
   
-//TOTALES
-  public lineChartDataT: Array<any> = [{ data: [this.lineChartData[0].data + this.lineChartData[1].data + this.lineChartData[2].data ]}]; //sumar arreglos numéricos elemento por elemento
-
+  //TOTALES
+  public lineChartDataT:Array<any> = [{ data:[], label: "Total Diario" }];
+  
 public lineChartColorsT:Array<any> = [
   { 
     backgroundColorT: 'rgba(255,255,255,1)',
-    borderColorT: 'rgba(155,155,155,1)',
+    borderColorT: 'rgba(55,155,255,1)',
     pointBackgroundColorT: 'rgba(155,155,155,1)',
     pointBorderColorT: '#fff',
     pointHoverBackgroundColorT: '#fff',
     pointHoverBorderColorT: 'rgba(148,159,177,0.8)'
   },
 ];
-public lineChartLegendT:boolean = true;
-public lineChartTypeT: string = 'line';
 
+  constructor() {
 
+    var aux = 0;
+    var arr = [];
+    for (let j = 0; j < this.lineChartData[0].data.length; j++) {
+      aux = this.lineChartData[0].data[j] + this.lineChartData[1].data[j] + this.lineChartData[2].data[j];
+      arr.push(aux);
+      this.lineChartDataT[0].data.push(aux);
+    }
+    //this.lineChartDataT[0].val = arr; 
+    console.log(this.lineChartDataT[0].data)
+  }
   
-  public randomize():void {
+/*  public randomize():void {
     let _lineChartData:Array<any> = new Array(this.lineChartData.length);
     for (let i = 0; i < this.lineChartData.length; i++) {
       _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
@@ -101,7 +90,7 @@ public lineChartTypeT: string = 'line';
     }
     this.lineChartData = _lineChartData;
   }
-  
+  */
   // events
   public chartClicked(e:any):void {
     console.log(e);
@@ -110,6 +99,4 @@ public lineChartTypeT: string = 'line';
   public chartHovered(e:any):void {
     console.log(e);
   }
-
-
 }
