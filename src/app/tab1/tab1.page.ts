b01bc818561be0501e674c4f58f6a1081fbd35fa
mcg31 <mcg-pna@gmail.com>
2020-10-12 02:05:59 -0300

    TercerCommit:TP_Final_pasandoA_APIrest

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
- // user: string = "";
-  constructor() { }
-
+  user: string = "";
+  private contrasenia: string;
+  private usuarios: [{ "Nombre": "Ignacio", "contraseña": "Ignacio" }, { "Nombre": "José González", "contraseña": "JoseG" }, { "Nombre": "José Brizio", "contraseña": "JoseB" }];
+  
+  constructor(private alContrl: AlertController) { }
+  
+  public login() {
+    for (var i; i < 3; i++) {
+      
+      if (this.usuarios[i].Nombre == this.user && this.usuarios[i].contraseña == this.contrasenia) {
+        const cuerpoAlerta = {
+          header: "Ingreso",
+          message: "Ingreso exitoso como usuario: " + this.user,
+          buttons: ["OK"]
+        };
+        const alerta = this.alContrl.create(cuerpoAlerta);
+      }
+    }
+  }
 }
